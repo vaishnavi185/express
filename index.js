5561906ccf664c63a51ec15d4a453a1bf2acf7df
vaishnavi185 <vishu032004@gmail.com>
2024-05-18 14:20:36 +0530

    handle bar

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+var expbh = require('express-handlebars')
+app.engine('handlebars', engine());
+
 app.use(express.static(path.join(__dirname, 'static')));
 app.use('/', require(path.join(__dirname, 'router', 'blogs.js')));
 
